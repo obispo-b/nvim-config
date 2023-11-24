4b93d7ee55591fa287a585becac0c62ffa88cab8
Bruno <brunoobispo@duck.com>
2023-11-24 01:30:44 -0300

    feat: added prettier to null-ls config

--- after/plugin/none-ls.lua
+++ after/plugin/none-ls.lua
@@ -1,9 +1,19 @@
 local null_ls = require("null-ls")
 
+local builtin = null_ls.builtins
+
+local sources = {
+	-- webdev stuff
+	builtin.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+	builtin.formatting.prettier.with({ filetypes = { "html", "markdown", "css", "javascript" } }), -- so prettier works only on these filetypes
+
+	-- Lua
+	builtin.formatting.stylua,
+
+	-- cpp
+	builtin.formatting.clang_format,
+}
+
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.completion.spell,
-	},
+	sources = sources,
 })
